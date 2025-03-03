4ac510582313df0ba215ddbef08e8045fe95b178
Tony Theodoropoulos <theodot@outlook.com>
2025-03-03 14:22:46 -0500

    completed demo

--- BallDodgeTemplate/GameScreen.Designer.cs
+++ BallDodgeTemplate/GameScreen.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.components = new System.ComponentModel.Container();
             this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.liveLabel = new System.Windows.Forms.Label();
+            this.pointsLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // gameTimer
@@ -38,11 +40,33 @@
             this.gameTimer.Interval = 20;
             this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
             // 
+            // liveLabel
+            // 
+            this.liveLabel.AutoSize = true;
+            this.liveLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.liveLabel.Location = new System.Drawing.Point(22, 15);
+            this.liveLabel.Name = "liveLabel";
+            this.liveLabel.Size = new System.Drawing.Size(93, 24);
+            this.liveLabel.TabIndex = 0;
+            this.liveLabel.Text = "livesLabel";
+            // 
+            // pointsLabel
+            // 
+            this.pointsLabel.AutoSize = true;
+            this.pointsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pointsLabel.Location = new System.Drawing.Point(456, 15);
+            this.pointsLabel.Name = "pointsLabel";
+            this.pointsLabel.Size = new System.Drawing.Size(106, 24);
+            this.pointsLabel.TabIndex = 1;
+            this.pointsLabel.Text = "pointsLabel";
+            // 
             // GameScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.Controls.Add(this.pointsLabel);
+            this.Controls.Add(this.liveLabel);
             this.DoubleBuffered = true;
             this.Name = "GameScreen";
             this.Size = new System.Drawing.Size(600, 500);
@@ -50,11 +74,14 @@
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.GameScreen_KeyUp);
             this.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.GameScreen_PreviewKeyDown);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label liveLabel;
+        private System.Windows.Forms.Label pointsLabel;
     }
 }
--- BallDodgeTemplate/GameScreen.cs
+++ BallDodgeTemplate/GameScreen.cs
@@ -13,11 +13,14 @@ namespace BallDodgeTemplate
 {
     public partial class GameScreen : UserControl
     {
+        public static int lives = 3;
+        public static int points = 0;
+
         public static int screenWidth;
         public static int screenHeight;
 
         bool leftArrowDown, rightArrowDown, upArrowDown, downArrowDown;
-        
+
         Ball chaseBall;
         List<Ball> balls = new List<Ball>();
         Player hero;
@@ -31,14 +34,14 @@ namespace BallDodgeTemplate
             InitializeComponent();
 
             screenWidth = this.Width;
-            screenHeight = this.Height; 
+            screenHeight = this.Height;
 
             InitializeGame();
         }
-        
+
         public void InitializeGame()
         {
-            hero = new Player();    
+            hero = new Player();
 
             int x = randGen.Next(20, this.Width - 50);
             int y = randGen.Next(20, this.Height - 50);
@@ -47,11 +50,7 @@ namespace BallDodgeTemplate
 
             for (int i = 0; i < 5; i++)
             {
-                x = randGen.Next(20, this.Width - 50);
-                y = randGen.Next(20, this.Height - 50);
-
-                Ball b = new Ball(x, y, 8, 8);
-                balls.Add(b);
+                CreateBall();
             }
         }
 
@@ -95,23 +94,82 @@ namespace BallDodgeTemplate
 
         private void gameTimer_Tick(object sender, EventArgs e)
         {
+            #region move code
+            // moving the hero
+            if (rightArrowDown == true)
+            {
+                hero.Move("right");
+            }
+
+            if (leftArrowDown == true)
+            {
+                hero.Move("left");
+            }
+
+            if (upArrowDown == true)
+            {
+                hero.Move("up");
+            }
+
+            if (downArrowDown == true)
+            {
+                hero.Move("down");
+            }
+
+            //moving the chase ball
             chaseBall.Move();
 
+            //moving enemies
             foreach (Ball b in balls)
             {
                 b.Move();
+            }
+            #endregion
+
+            if (hero.Collision(chaseBall))
+            {
+                points++;
+
+                CreateBall();
+            }
+
+            foreach (Ball b in balls)
+            {
+                if (hero.Collision(b))
+                {
+                    lives--;
+                }
+            }
+
+            if (lives == 0)
+            {
+                gameTimer.Stop();
             }
 
             Refresh();
         }
 
+        private void CreateBall()
+        {
+            int x = randGen.Next(20, this.Width - 50);
+            int y = randGen.Next(20, this.Height - 50);
+
+            Ball b = new Ball(x, y, 8, 8);
+            balls.Add(b);
+        }
+
         private void GameScreen_Paint(object sender, PaintEventArgs e)
         {
+            //update labels
+            liveLabel.Text = $"Lives: {lives}";
+            pointsLabel.Text = $"Points: {points}";
+
+
             //chaseball
             e.Graphics.FillEllipse(greenBrush, chaseBall.x, chaseBall.y, chaseBall.size, chaseBall.size);
 
             //balls to avoid
-            foreach(Ball b in balls)
+            foreach (Ball b in balls)
             {
                 e.Graphics.FillEllipse(redBrush, b.x, b.y, b.size, b.size);
             }
